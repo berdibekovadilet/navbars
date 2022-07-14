4be5c62b874fdb32877de39535c81734c284e5b0
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-07-14 18:21:23 +0600

    added first navbar

--- src/components/Navbar1.jsx
+++ src/components/Navbar1.jsx
@@ -1,15 +1,40 @@
 import styles from "../styles/navbar1.module.css";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { useRef } from "react";
 
 const Navbar1 = () => {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsiveNav");
+  };
   return (
-    <div className={styles.container}>
-      <div className={styles.logo}>Logo.</div>
-      <ul className={styles.menuList}>
-        <li>item 1</li>
-        <li>item 2</li>
-        <li>item 3</li>
-      </ul>
-    </div>
+    <header className={styles.container}>
+      <h3 className={styles.logo}>Logo.</h3>
+      <nav ref={navRef} className={styles.menuList}>
+        <a className={styles.listItem} href="#/">
+          item 1
+        </a>
+        <a className={styles.listItem} href="#/">
+          item 2
+        </a>
+        <a className={styles.listItem} href="#/">
+          item 3
+        </a>
+        <a className={styles.listItem} href="#/">
+          item 4
+        </a>
+        <button
+          className={`${styles.navBtn} ${styles.navCloseBtn}`}
+          onClick={showNavbar}
+        >
+          <FaTimes />
+        </button>
+      </nav>
+      <button className={styles.navBtn} onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
   );
 };
 
